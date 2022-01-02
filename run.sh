2fa9eefdd0d0494ab1554cf78231121e42de068d
oshaboy <35503208+oshaboy@users.noreply.github.com>
2022-01-02 14:52:29 +0200

    g

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-gcc -O3 -shared -fPIC -o sdl_gawk.so sdl_gawk.c -lSDL2 && AWKLIBPATH=$(pwd) awk -lsdl_gawk -f sdl_demo.awk
+gcc -O3 -shared -fPIC -o sdl_gawk.so sdl_gawk.c -lSDL2 && AWKLIBPATH=$(pwd) gawk -lsdl_gawk -f sdl_demo.awk
